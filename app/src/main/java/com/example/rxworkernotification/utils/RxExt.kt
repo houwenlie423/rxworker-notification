6e3839854fe0c071d2f7c9e55557fa283ed9c8eb
Houwen Lie <houwenlie98@gmail.com>
2023-07-12 23:19:50 +0700

    Add RxAndroid to dependency and create ext function for completable to apply schedulers

--- app/src/main/java/com/example/rxworkernotification/utils/RxExt.kt
+++ app/src/main/java/com/example/rxworkernotification/utils/RxExt.kt
@@ -1,7 +1,10 @@
 package com.example.rxworkernotification.utils
 
 import io.reactivex.Completable
+import io.reactivex.Scheduler
+import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.observers.DisposableCompletableObserver
+import io.reactivex.schedulers.Schedulers
 
 
 /**
@@ -9,11 +12,16 @@ import io.reactivex.observers.DisposableCompletableObserver
  * @version RxExt, v 0.1 Wed 7/12/2023 11:05 PM by Houwen Lie
  */
 
+fun Completable.applySchedulers(
+    threadScheduler: Scheduler = Schedulers.io(),
+    postThreadScheduler: Scheduler = AndroidSchedulers.mainThread()
+) = this.subscribeOn(threadScheduler).observeOn(postThreadScheduler)
+
 fun Completable.subscribeByAutoDispose(
     onComplete: () -> Unit,
     onError: (Throwable) -> Unit
 ) {
-    this.subscribe(object: DisposableCompletableObserver() {
+    this.subscribe(object : DisposableCompletableObserver() {
         override fun onComplete() {
             onComplete()
             dispose()
